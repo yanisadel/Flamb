421b50a269292b5e311280bfbeb4bd4fe953228b
yanisadel <yanis_adel@hotmail.com>
2023-01-09 14:21:52 +0100

    Add comments to no_grad

--- flamb/autograd/grad_mode.py
+++ flamb/autograd/grad_mode.py
@@ -1,12 +1,20 @@
+"""
+This file contains a class allowing to use a context where gradient is not computed
+"""
+
 import flamb
 
 
 class no_grad:
+    """
+    Context that disables gradient computation
+    """
+
     def __init__(self):
         pass
 
     def __enter__(self):
-        flamb.environ["compute_grad"] = False
+        flamb.environ["grad_enabled"] = False
 
     def __exit__(self, type, value, traceback):
-        flamb.environ["compute_grad"] = True
+        flamb.environ["grad_enabled"] = True
