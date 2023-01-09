ed5c75017b5919aa1cd75dc45c5631e137f5e4a4
yanisadel <yanis_adel@hotmail.com>
2023-01-09 15:21:30 +0100

    Add test for flamb.no_grad

--- tests/autograd/test_grad_mode.py
+++ tests/autograd/test_grad_mode.py
@@ -1,12 +1,18 @@
 import flamb
 from flamb import Variable
 
+
 def test_no_grad():
     x = Variable(4, requires_grad=True)
     with flamb.no_grad():
-        assert flamb.environ['compute_grad'] == False
-        y = x**2
+        assert flamb.environ["is_grad_enabled"] == False
+        y = x ** 2
+
+    assert y.requires_grad == False, "y.requires_grad must be False"
+    assert (
+        flamb.environ["is_grad_enabled"] == True
+    ), "flamb.environ['is_grad_enabled'] should has turned back to True"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test_no_grad()
