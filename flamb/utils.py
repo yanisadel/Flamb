50498fe1afd0d279a83e231e9145bae50ceb6a57
yanisadel <yanis_adel@hotmail.com>
2023-01-09 15:11:33 +0100

    Comment flamb.utils functions

--- flamb/utils.py
+++ flamb/utils.py
@@ -1,17 +1,23 @@
 import flamb
 
+
 def convert_variable(var):
     """
-    Convert a Variable into a value (integer or float). Also works if var is not a variable
+    Convert a flamb.Variable into a value (integer or float). 
+    Also works if var is an int or a float
     """
     if isinstance(var, flamb.Variable):
         return var.value
 
     elif isinstance(var, (int, float)):
         return var
-    
+
     else:
         raise Exception(f"Cannot convert {type(var)} to a value")
 
+
 def convert_variable_list(l):
+    """
+    Works the same way as convert_variable but for a list of variables
+    """
     return [convert_variable(var) for var in l]
