f0d1dc3a22c6d6016505a551517b52d9605c3ff4
yanisadel <yanis_adel@hotmail.com>
2023-01-09 14:45:15 +0100

    Change compute_grad to grad_enabled in flamb.environ

--- flamb/__init__.py
+++ flamb/__init__.py
@@ -1,6 +1,6 @@
 from flamb.autograd import Variable, no_grad, functional
 from flamb.tensor import Tensor
 
-environ = {'compute_grad': True}
+environ = {"grad_enabled": True}
 
-__all__ = ['Variable', 'Tensor', 'no_grad', 'environ', 'functional']
+__all__ = ["Variable", "Tensor", "no_grad", "environ", "functional"]
