0c64adc87038525cad642f60fea063abb1147fd3
yanisadel <yanis_adel@hotmail.com>
2023-01-09 03:13:12 +0100

    Improve function to convert Variable into a int or float

--- flamb/utils.py
+++ flamb/utils.py
@@ -1,13 +1,17 @@
+import flamb
+
 def convert_variable(var):
     """
     Convert a Variable into a value (integer or float). Also works if var is not a variable
     """
-    try:
-        res = var.value
-        return res
+    if isinstance(var, flamb.Variable):
+        return var.value
 
-    except:
+    elif isinstance(var, (int, float)):
         return var
+    
+    else:
+        raise Exception(f"Cannot convert {type(var)} to a value")
 
 def convert_variable_list(l):
     return [convert_variable(var) for var in l]
