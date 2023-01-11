6ae4d411586b66d0774a11a9222c051c5cdcd0e3
yanisadel <yanis_adel@hotmail.com>
2023-01-11 19:13:11 +0100

    Add comments and tests

--- flamb/tensor/_functions.py
+++ flamb/tensor/_functions.py
@@ -4,24 +4,31 @@ import random
 from .utils import *
 
 def zeros(shape, dtype=object, requires_grad=False):
+    """Creates a tensor composed of zeros"""
     tensor = flamb.Tensor(shape, dtype=dtype)
     for index in loop_on_indicies(shape):
         tensor[index] = flamb.Variable(0, requires_grad=requires_grad)
     return tensor
 
+
 def ones(shape, dtype=object, requires_grad=False):
+    """Creates a tensor composed of ones"""
     tensor = flamb.Tensor(shape, dtype=dtype)
     for index in loop_on_indicies(shape):
         tensor[index] = flamb.Variable(1, requires_grad=requires_grad)
     return tensor
 
+
 def rand(shape, dtype=object, requires_grad=False):
+    """Creates a tensor composed of random values between -1 and 1"""
     tensor = flamb.Tensor(shape, dtype=dtype)
     for index in loop_on_indicies(shape):
         tensor[index] = flamb.Variable(random.uniform(-1, 1), requires_grad=requires_grad)
     return tensor
 
-def to_tensor(l, requires_grad=False, dtype=object):
+
+def to_tensor(l, dtype=object, requires_grad=False):
+    """Convert an array-like object (a list or a numpy array) to a tensor"""
     l = np.array(l, dtype=dtype)
     shape = l.shape
     tensor = flamb.Tensor(shape, dtype=dtype)
@@ -32,12 +39,18 @@ def to_tensor(l, requires_grad=False, dtype=object):
             tensor[index] = l[index]
     return tensor
 
+
 def matmul(a, b):
+    """Computes the matrix multiplication of a and b"""
     return np.matmul(a, b)
+
     
 def dot(a, b):
+    """Computes the dot product of a and b"""
     return np.dot(a, b)
 
+
 def concatenate(a, b, axis=None):
+    """Concatenates the tensors a and b"""
     return np.concatenate((a, b), axis=axis)
     
--- tests/tensor/test_functions.py
+++ tests/tensor/test_functions.py
@@ -1,6 +1,7 @@
 import flamb
 
 def test_zeros():
+    """Test the function that generates a tensor of zeros"""
     shape = (2, 2)
     tensor = flamb.zeros(shape)
     assert tensor.shape == shape, "Shape is not correct"
@@ -10,6 +11,7 @@ def test_zeros():
 
 
 def test_ones():
+    """Test the function that generates a tensor of ones"""
     shape = (2, 2)
     tensor = flamb.ones(shape)
     assert tensor.shape == shape, "Shape is not correct"
@@ -18,6 +20,33 @@ def test_ones():
             assert tensor[i][j] == 1
 
 
+def test_to_tensor():
+    """Test the function that converts an array-like to a tensor"""
+    l = [ [1,2],
+          [3,4]]
+    tensor = flamb.to_tensor(l)
+
+    assert isinstance(tensor, flamb.Tensor)
+    assert tensor.shape == (2,2)
+    for i in range(2):
+        for j in range(2):
+            assert tensor[i][j] == l[i][j]
+
+
+def test_concatenate():
+    """Test the function that concatenates two tensors"""
+    a = flamb.to_tensor([1,2])
+    b = flamb.to_tensor([3,4])
+    res = flamb.concatenate(a, b)
+    target = flamb.to_tensor([1,2,3,4])
+
+    assert res.shape == (4,)
+    for i in range(4):
+        assert res[i] == target[i]
+    
+
 if __name__ == "__main__":
     test_zeros()
     test_ones()
+    test_to_tensor()
+    test_concatenate()
